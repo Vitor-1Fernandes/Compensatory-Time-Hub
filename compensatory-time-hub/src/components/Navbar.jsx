eb197616d1a0ec4eeccd071b0447228981d64804
vitor-1fernandes <vitorfernandes11122006@gmail.com>
2026-03-05 17:28:20 -0300

    New bg e navbar

--- compensatory-time-hub/src/components/Navbar.jsx
+++ compensatory-time-hub/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faClock } from "@fortawesome/free-solid-svg-icons"
 import {faSignOut} from "@fortawesome/free-solid-svg-icons"
 import {faUser} from "@fortawesome/free-solid-svg-icons"
 
@@ -8,18 +7,18 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
-        <div className="flex items-center justify-between w-full bg-linear-600 from-slate-800 via-[#23324b] to-slate-800 text-white font-bold py-5 px-10"
+        <div className="flex items-center justify-between w-full text-[#ffffff] font-bold py-5 px-4 md:px-10" 
         >
-            <div className='flex items-center gap-3'>
-                <FontAwesomeIcon className="text-3xl" icon={faClock} />
-                <p className='text-xl'>Banco de horas</p>
+            <div className='flex flex-col-reverse items-start gap-1 pl-12'>
+                <h1 className="flex w-auto font-light"> Olá, Vitor - Aqui está seu resumo</h1>
+                <p className='lg:text-xl bg-clip-text text-transparent bg-linear-to-l from-[#6c8cde] via-[#5b9be5] to-[#8aa3e1]'>Banco de horas</p>
             </div>
 
 
             <div className="flex gap-5">
 
-                <Link to="/user"><FontAwesomeIcon className="text-3xl" icon={faUser}/></Link>
-                <Link to="/"><FontAwesomeIcon className="text-3xl" icon={faSignOut}/></Link>
+                <Link to="/user"><FontAwesomeIcon className="lg:text-xl" icon={faUser}/></Link>
+                <Link to="/"><FontAwesomeIcon className="lg:text-xl" icon={faSignOut}/></Link>
             </div>
 
         </div>
