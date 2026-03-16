4b1c4f1c028b16ba8d3ea2ff71c7dcaa24178aaa
vitor-1fernandes <vitorfernandes11122006@gmail.com>
2026-03-16 14:51:01 -0300

    Criação de lógica para cálculo de horas extras de acordo com fechamento da folha

--- compensatory-time-hub/src/components/CardTime.jsx
+++ compensatory-time-hub/src/components/CardTime.jsx
@@ -5,8 +5,38 @@ import { useEffect } from 'react'
 
 function CardTime(props) {
 
+    let today = new Date()
+    const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+
+    console.log(today.getDate())
+
+    let primeiraQuinzena = true
+
+    if(15 <= 13){primeiraQuinzena = true}else{primeiraQuinzena=false}
+        
+    let currentMonth = (primeiraQuinzena ? (today.getMonth() + 1) - 1 : (today.getMonth() + 1))
+    let currentMonth2 = (primeiraQuinzena ? (today.getMonth() + 1) : (today.getMonth() + 1) + 1)
+
+    const returnTime = (timeExit, timeEntry, projeto, dia) => {
+
+        let time = (Number(String(timeExit.split(":")[0]*60)) + Number(String(timeExit.split(":")[1]))) - (Number(String((timeEntry.split(":")[0]*60))) + Number(String((timeEntry.split(":")[1]))))
+
+        if(time > 6*60){time = time - 60}
+
+        if(projeto != "Horas Extra"){dia != 5 ? time = 0 - 9*60 : time = 0 - 8*60}
+
+        if(projeto == "Folga - Descontar do Banco"){dia != 5 ? time = 0 - 9*60 : time = 0 - 8*60}
+
+        if(projeto == "Horas Negativas"){time = (Number(String((timeEntry.split(":")[0]*60))) + Number(String((timeEntry.split(":")[1]))))}
+
+        if(projeto == "Horas Positivas"){time = (Number(String((timeEntry.split(":")[0]*60))) + Number(String((timeEntry.split(":")[1]))))}
+
+        return time
+
+    }
 
     const sumBank = (array) => {
+
         let totalHours = 0
         let payTotalHours = 0
 
@@ -14,42 +44,22 @@ function CardTime(props) {
         let payTextTime = ""
 
         let time = 0
-        let payTime = 0
 
         for (let days = 0; days < array.length; days++) {
-            if (array[days].project == "Horas Extra") {
-                payTime = (Number(String(array[days].timeExit.split(":")[0] * 60)) + Number(String(array[days].timeExit.split(":")[1]))) - (Number(String((array[days].timeEntry.split(":")[0] * 60))) + Number(String((array[days].timeEntry.split(":")[1]))))
 
-                if (payTime > 6 * 60) {
-                    payTime = payTime - 60
+            let date = new Date(array[days].date)
+
+                time = returnTime(array[days].timeExit, array[days].timeEntry, array[days].project, (date.getDay() + 1))                
+
+                if(array[days].project == "Horas Extra"){
+                    if(((date.getMonth()+1) == currentMonth && (date.getDate()+1 > 13) && date.getFullYear() == today.getFullYear()) || ((date.getMonth()+1) == currentMonth2 && (date.getDate()+1) <= 13) && date.getFullYear() == today.getFullYear()){
+                        payTotalHours = payTotalHours + time
+                    }
+                }else{
+                    totalHours = totalHours + time
                 }
-                payTotalHours = payTotalHours + payTime
+                
             }
-            if (array[days].project == "Banco de Horas" || array[days].project == "Folga - Descontar do Banco" || array[days].project == "Horas Positivas" || array[days].project == "Horas Negativas") {
-
-                time = (Number(String(array[days].timeExit.split(":")[0] * 60)) + Number(String(array[days].timeExit.split(":")[1]))) - (Number(String((array[days].timeEntry.split(":")[0] * 60))) + Number(String((array[days].timeEntry.split(":")[1]))))
-
-                if (time <= 6 * 60) {
-                    time = time - 9 * 60
-                }
-                if (time > 6 * 60) {
-                    time = time - 10 * 60
-                }
-                if(array[days].project == "Folga - Descontar do Banco"){
-                    time = 0 - 9*60
-                }if(array[days].project == "Horas Negativas"){
-
-                    time = 0 - (Number(String((array[days].timeEntry.split(":")[0] * 60))) + Number(String((array[days].timeEntry.split(":")[1]))));
-                }
-                if(array[days].project == "Horas Positivas"){
-
-                    time = 0 + (Number(String((array[days].timeEntry.split(":")[0] * 60))) + Number(String((array[days].timeEntry.split(":")[1]))));
-
-                }
-                totalHours = totalHours + time
-            }
-
-        }
         
         payTextTime = String(Math.floor(payTotalHours / 60)) + "h" + String((payTotalHours % 60))
         if (totalHours < 0) {
@@ -59,9 +69,7 @@ function CardTime(props) {
             textTime = Math.floor(totalHours/ 60) + "h" + Math.abs((totalHours % 60))
         }
 
-
-        return [textTime, totalHours, payTextTime, payTotalHours]
-    }
+        return [textTime, totalHours, payTextTime, payTotalHours]}
 
     let hoursList= sumBank(props.workDaysList)
     let textTime = hoursList[0]
@@ -81,11 +89,12 @@ function CardTime(props) {
             {!!payTotalHours && (<div className="flex flex-wrap w-full lg:w-auto lg:max-w-85 bg-[#15191E] rounded-lg p-5 text-white shadow-[#000000] shadow-lg">
                 <FontAwesomeIcon className="mb-2 text-[#FFD700] p-1 rounded-md" icon={faDollarSign} />
                 <p className="w-full text-sm">Horas Extras</p>
-                <h2 className="w-full font-bold text-3xl text-[#FFD700]">{payTextTime}</h2><p className="text-sm w-full"> Referentes a 14/03 a 13/04 </p></div>)}
+                <h2 className="w-full font-bold text-3xl text-[#FFD700]">{payTextTime}</h2><p className="text-sm w-full"> Período de 14/{currentMonth} a 13/{currentMonth2} </p>
 
-        </div>
+                <p className="text-sm font-light w-full">Pagamento em {months[currentMonth + 1]} </p>
+ 
+                </div>)}
 
-    )
-}
+        </div>)}
 
 export default CardTime;
