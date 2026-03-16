183f05cf76b336b0b4f7a189d0cb7a1c32f95478
vitor-1fernandes <vitorfernandes11122006@gmail.com>
2026-03-16 14:50:33 -0300

    Otimização de função para lidar com cálculo de horas

--- compensatory-time-hub/src/components/DailyCard.jsx
+++ compensatory-time-hub/src/components/DailyCard.jsx
@@ -7,71 +7,54 @@ import { faPencil } from "@fortawesome/free-solid-svg-icons"
 
 function DailyCard(props){
 
-    let workTime = 0;
+    const returnTime = (timeExit, timeEntry, projeto, dia) => {
+
+        let time = (Number(String(timeExit.split(":")[0]*60)) + Number(String(timeExit.split(":")[1]))) - (Number(String((timeEntry.split(":")[0]*60))) + Number(String((timeEntry.split(":")[1]))))
+
+        if(time > 6*60){time = time - 60}
+
+        if(projeto != "Horas Extra"){dia != 5 ? time = 0 - 9*60 : time = 0 - 8*60}
+
+        if(projeto == "Folga - Descontar do Banco"){dia != 5 ? time = 0 - 9*60 : time = 0 - 8*60}
+
+        if(projeto == "Horas Negativas"){time = (Number(String((timeEntry.split(":")[0]*60))) + Number(String((timeEntry.split(":")[1]))))}
+
+        if(projeto == "Horas Positivas"){time = (Number(String((timeEntry.split(":")[0]*60))) + Number(String((timeEntry.split(":")[1]))))}
+
+        return time
+
+    }
+
+    let date = new Date(props.date)
+    console.log(date)
+    const time = returnTime(props.timeExit, props.timeEntry, props.project, date.getDay() + 1);
+
+    let payTextTime = ""
     let textTime = ""
 
-     const time = (Number(String(props.timeExit.split(":")[0]*60)) + Number(String(props.timeExit.split(":")[1]))) - (Number(String((props.timeEntry.split(":")[0]*60))) + Number(String((props.timeEntry.split(":")[1]))));
-
-    
-    if (Number(time) <= 6*60){
-        workTime = ((Number(String(props.timeExit.split(":")[0]*60)) + Number(String(props.timeExit.split(":")[1]))) - (Number(String((props.timeEntry.split(":")[0]*60))) + Number(String((props.timeEntry.split(":")[1])))) - 9*60)
-    }
-    if (Number(time)  > 6*60){
-        workTime =  ((Number(String(props.timeExit.split(":")[0]*60)) + Number(String(props.timeExit.split(":")[1]))) - (Number(String((props.timeEntry.split(":")[0]*60))) + Number(String((props.timeEntry.split(":")[1])))) - 10*60)
-    }
-    if(props.project == "Folga - Descontar do Banco"){
-
-        workTime = 0 - 9*60
-    }
-    if(props.project == "Horas Negativas"){
-
-        workTime = 0 - (Number(String((props.timeEntry.split(":")[0]*60))) + Number(String((props.timeEntry.split(":")[1]))));
-    }
-    if(props.project == "Horas Positivas"){
-
-        workTime = 0 +  (Number(String((props.timeEntry.split(":")[0]*60))) + Number(String((props.timeEntry.split(":")[1]))));
-    }
-
-    let payWorkTime = 0
-    let payTextTime = ""
-
-
-    let payTime = (Number(String(props.timeExit.split(":")[0]*60)) + Number(String(props.timeExit.split(":")[1]))) - (Number(String((props.timeEntry.split(":")[0]*60))) + Number(String((props.timeEntry.split(":")[1]))));
-
-    if (Number(payTime) > 6*60){
-        payWorkTime = ((Number(String(props.timeExit.split(":")[0]*60)) + Number(String(props.timeExit.split(":")[1]))) - (Number(String((props.timeEntry.split(":")[0]*60))) + Number(String((props.timeEntry.split(":")[1])))) - 60)
-    }else{
-        payWorkTime = payTime
-    }
-   
-    payTextTime = Math.floor(payWorkTime/60) + "h" + (payWorkTime % 60)
+    payTextTime = Math.floor(time/60) + "h" + (time % 60)
      
-
-
-    if(workTime < 0){
-        textTime = Math.ceil(workTime/60) + "h" + Math.abs((workTime % 60))
+    if(time < 0){
+        textTime = Math.ceil(time/60) + "h" + Math.abs((time % 60))
         
     }else{
-        textTime = Math.floor(workTime/60) + "h" + Math.abs((workTime % 60))
+        textTime = Math.floor(time/60) + "h" + Math.abs((time % 60))
     }
 
     
-    
-    
-
     return(
         
          <div className="flex flex-wrap justify-between  bg-[#15191E] rounded-lg p-5 gap-1 text-white shadow-[#000000] shadow-sm">
            <div>
-            <p className="w-full text-sm"><FontAwesomeIcon icon={faCalendar}/>{props.date}</p>
-            <h2 className={workTime >= 0 || props.project == "Horas Extra" ? "text-blue-400 font-bold" : "text-red-400 font-bold"}>{props.name}</h2>
+            <p className="w-full text-sm"><FontAwesomeIcon icon={faCalendar}/>{date.toLocaleDateString('pt-BR')}</p>
+            <h2 className={time >= 0 || props.project == "Horas Extra" ? "text-blue-400 font-bold" : "text-red-400 font-bold"}>{props.name}</h2>
             <p className="w-full text-sm"> <FontAwesomeIcon icon={faSuitcase}/> {props.project}</p>
-            <p className={workTime >= 0 || props.project == "Horas Extra" ? "text-blue-400 text-sm" : "text-red-400 text-sm"}> <FontAwesomeIcon icon={faClock}/> {props.timeEntry} - {props.timeExit}</p>
+            <p className={time >= 0 || props.project == "Horas Extra" ? "text-blue-400 text-sm" : "text-red-400 text-sm"}> <FontAwesomeIcon icon={faClock}/> {props.timeEntry} - {props.timeExit}</p>
             </div>
             <div className='flex flex-col justify-between
             '>
             <div className='text-3xl flex justify-end items-center'>
-                <p className={workTime >= 0 || props.project == "Horas Extra" ? "text-blue-400 font-bold" : "text-red-400 font-bold"}>{workTime >= 0 || props.project == "Horas Extra"? "+" : ""}{props.project == "Horas Extra" ? payTextTime : textTime}</p>
+                <p className={time >= 0 || props.project == "Horas Extra" ? "text-blue-400 font-bold" : "text-red-400 font-bold"}>{time >= 0 || props.project == "Horas Extra"? "+" : ""}{props.project == "Horas Extra" ? payTextTime : textTime}</p>
             </div>
             <div className='flex justify-end text-lg gap-5'>
                 <button onClick={(e) => props.delete(e, props)}><FontAwesomeIcon icon={faTrash}/></button>
